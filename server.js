6ee259ad1ac50fec7f5a621853f1ac6b4f9cddc8
Will Vining <awvining91@gmail.com>
2023-01-12 15:18:26 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,64 @@
+
+const express = require("express");
+const path = require("path");
+
+//Apollo Server Import 
+const { ApolloServer } = require("apollo-server-express");
+
+
+//Resolver and typeDefs
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+
+
+//Database
+const db = require("./config/connection");
+
+
+
+//Express
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+
+//Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+
+//Use Express with Apollo 
+server.applyMiddleware({ app });
+
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
+app.use(express.static(buildPath));
+
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+
+
+//Get All
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(` The server for the API is at port ${PORT}!`);
+    console.log(`Please use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
