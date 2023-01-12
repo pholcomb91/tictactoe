63c88db2943813e04e0c52baf86a8bf16721d52a
Will Vining <awvining91@gmail.com>
2023-01-12 15:42:43 -0700

    added schema folder and index, typeDef and resolver files

---
+++ schemas/resolvers.js
@@ -0,0 +1,15 @@
+const {  } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+       
+    },
+    Mutation: {
+        
+        }
+    }
+  };
+  
+  module.exports = resolvers;
