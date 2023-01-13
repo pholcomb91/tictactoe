d55edb0cd72bca6d25bb11a5cf0ccd4e57d48794
jlmayo <jlmayo22@gmail.com>
2023-01-12 21:20:23 -0700

    Resolvers and typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,22 +2,28 @@ const { gql } = require('apollo-server-express');
 
 // typeDefs
 const typeDefs = gql`
-    type  {
-        _id: 
-       
-    }
     type User {
         _id: ID
         username: String
         email: String
-             
+        wins: Int
+        losses: Int
     }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
     type Query {
         me: User
     }
  
     type Mutation {
-       
+       addUser(username: String!, email: String!, password: String!): Auth
+       updateUser(username: String! email: String! password: String!): User
+       login(email: String!, password: String!): Auth
+       updateScore(username: String!, score: String!): User
 }
 `;
 
