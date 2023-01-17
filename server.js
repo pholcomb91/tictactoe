4a7f90d6c8d558dcf64abf62234ced91a0b29317
Will Vining <awvining91@gmail.com>
2023-01-16 20:28:30 -0700

    combined the index socket.io chat file in the folder file into server.js and deleted the index socket.io chat file

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require("express");
 const path = require("path");
 const { io } = require("socket.io-client");
 
+//Imports for chat socket.io
+const http = require("http");
+const cors = require("cors");
+
+
 //Apollo Server Import 
 const { ApolloServer } = require("apollo-server-express");
 
@@ -19,7 +24,7 @@ const db = require("./server/config/connection");
 
 //Express
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 
 
 //Apollo server
@@ -63,3 +68,42 @@ db.once("open", () => {
     console.log(`Please use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
+
+//Here is where we get socket.io
+const { Server } = require("socket.io");
+app.use(cors());
+
+
+
+//Creating a server to use socket.io
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+//Logic for joining a chat room
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+//Logic for sending a message
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
+
+//Hopefully the backend socket.io chat is working now on the server :)
